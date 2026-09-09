26b234619a37d05b33b594c9b25466c0f640abcc
FaisalAlanazi-1 <xhjoalenzi@gmail.com>
2026-09-09 18:49:33 +0300

    add env file for api key

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:faisal_alanazi_proj1/screens/splash/splash_screen.dart';
-import 'package:faisal_alanazi_proj1/service/database.dart';
 
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
 
   await Supabase.initialize(
     url: 'https://limvieaorumriikaffvq.supabase.co',
-    publishableKey: 'sb_publishable_MXqb_RcVSmOXHIuQTQgGZg_s4Y8Hwun',
+    publishableKey: dotenv.get('apiKey'),
   );
-// Database().inset() ; 
+  // Database().inset() ;
   runApp(MainApp());
 }
 
